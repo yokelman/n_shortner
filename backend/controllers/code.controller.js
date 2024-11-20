fae7215af6d09680e4f9271408c212f91d94401d
aayush4527f <aayushchhajed6@gmail.com>
2024-11-20 18:23:22 +0530

    fixed function repeatation

--- backend/controllers/code.controller.js
+++ backend/controllers/code.controller.js
@@ -2,17 +2,8 @@ import Code from '../models/code.model.js';
 
 import { authenticate, find_docs } from './common_functions.js';
 
-async function find_codes(filter){
-    try {
-        let code_found = await Code.find(filter);
-        return code_found;
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
 export const getCodes = async(req,res)=>{
-    let codes = await find_codes({});
+    let codes = await find_docs({},Code);
     res.status(200).json({success: true,codes:codes});
 }
 

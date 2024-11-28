d98952d9b63050e8b126f93a264cf08de9cfdfcf
aayush4527f <aayushchhajed6@gmail.com>
2024-11-28 23:26:09 +0530

    view all public codes

---
+++ static/script.js
@@ -0,0 +1,28 @@
+fetch('/api/code')
+    .then((data) => {
+        data.json()
+        .then((j_data) => {
+            create_code_elems(j_data.codes);
+        })
+    })
+
+function create_code_elems(codes) {
+    const codes_container = document.getElementById('codes_container');
+    codes.forEach(element => {
+        let padded_val = element.value.toString().padStart(6, '0');
+
+        let code_div = document.createElement('div');
+        let value = document.createElement('a');
+        let redirect = document.createElement('span');
+
+        redirect.innerText = element.redirect;
+        value.innerText = `click to visit: ${padded_val}`;
+        value.href = padded_val;
+
+        code_div.className = "w-full lg:w-1/4 md:w-1/3 sm:w-1/2 min-h-[25vh] border border-black flex flex-col justify-between overflow-hidden p-2 my-1";
+
+        code_div.appendChild(redirect);
+        code_div.appendChild(value);
+        codes_container.appendChild(code_div);
+    });
+}

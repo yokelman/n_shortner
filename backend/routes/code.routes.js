41d2501de15ddb05153ae17896f669a083595b6a
aayush4527f <aayushchhajed6@gmail.com>
2024-12-02 17:42:42 +0530

    changed reqest from get to post

--- backend/routes/code.routes.js
+++ backend/routes/code.routes.js
@@ -8,7 +8,7 @@ import { assignCode, deleteCode, getCodes } from '../controllers/code.controller
 const router = express.Router();
 
 // routes
-router.get('/:owner?',getCodes);
+router.post('/:owner?',getCodes);
 router.post('/assign',assignCode);
 // router.post('/update',updateCode);
 router.post('/delete',deleteCode);
